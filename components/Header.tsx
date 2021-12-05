7043b596784416d3663e1ef79e8af9e9ba8e0a5d
Masahiro Aoki <rafe.kun@gmail.com>
2021-12-06 00:52:38 +0900

    Headerを追加

---
+++ components/Header.tsx
@@ -0,0 +1,24 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+import { Fragment } from "react";
+
+export default function Header() {
+  return (
+    <Disclosure as="nav" className="bg-white shadow">
+      {({ open }) => (
+        <>
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex justify-between h-16">
+              <div className="flex">
+                <div className="flex-shrink-0 flex items-center">
+                  新型コロナ隔離者・情報交換掲示板
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </Disclosure>
+  );
+}
---
+++ components/Scaffold.tsx
@@ -0,0 +1,3 @@
+export default function Scaffold() {
+  return <body className="bg-gray-200 w-full h-full"></body>;
+}
