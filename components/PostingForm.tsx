e116767e297864bf09ee7147ab292829c4d87329
Masahiro Aoki <masahiroaoki@MasahironoMacBook-Pro.local>
2021-12-12 18:39:01 +0900

    投稿を追加、表示できるように変更

--- components/PostingForm.tsx
+++ components/PostingForm.tsx
@@ -1,4 +1,13 @@
+import { setDoc, doc, getFirestore, Timestamp } from "firebase/firestore";
+import Link from "next/link";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
 export default function PostingForm() {
+  const [username, setUsername] = useState("");
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
   return (
     <form className="space-y-8 divide-y divide-gray-200 m-10">
       <div className="space-y-8 divide-y divide-gray-200 sm:space-y-5">
@@ -16,34 +25,46 @@ export default function PostingForm() {
           <div className="mt-6 sm:mt-5 space-y-6 sm:space-y-5">
             <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
               <label
-                htmlFor="email"
+                htmlFor="text"
                 className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
               >
                 ユーザー名
               </label>
               <div className="mt-1 sm:mt-0 sm:col-span-2">
                 <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    console.log(value);
+                    setUsername(value);
+                  }}
+                  value={username}
+                  type="text"
+                  name="username"
+                  id="username"
+                  autoComplete="username"
                   className="block max-w-lg w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md"
                 />
               </div>
             </div>
             <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
               <label
-                htmlFor="email"
+                htmlFor="text"
                 className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
               >
                 タイトル
               </label>
               <div className="mt-1 sm:mt-0 sm:col-span-2">
                 <input
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    console.log(value);
+                    setTitle(value);
+                  }}
+                  value={title}
                   id="email"
                   name="email"
                   type="email"
-                  autoComplete="email"
+                  autoComplete="on"
                   className="block max-w-lg w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md"
                 />
               </div>
@@ -58,11 +79,16 @@ export default function PostingForm() {
               </label>
               <div className="mt-1 sm:mt-0 sm:col-span-2">
                 <textarea
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    console.log(value);
+                    setBody(value);
+                  }}
                   id="about"
                   name="about"
                   rows={3}
                   className="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md"
-                  defaultValue={""}
+                  defaultValue={body}
                 />
                 {/* <p className="mt-2 text-sm text-gray-500">
                   Tell me what happened to you.
@@ -122,18 +148,33 @@ export default function PostingForm() {
 
       <div className="pt-5">
         <div className="flex justify-end">
-          <button
-            type="button"
-            className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            キャンセル
-          </button>
-          <button
-            type="submit"
-            className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            投稿
-          </button>
+          <Link href="/">
+            <button
+              type="button"
+              className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              キャンセル
+            </button>
+          </Link>
+          <Link href="/">
+            <button
+              onClick={async () => {
+                console.log(username, title, body);
+                const threadId = uuidv4();
+                await setDoc(doc(getFirestore(), "threads-v1", threadId), {
+                  username: username,
+                  title: title,
+                  body: body,
+                  createdAt: Timestamp.now(),
+                  threadId: threadId,
+                });
+              }}
+              type="submit"
+              className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              投稿
+            </button>
+          </Link>
         </div>
       </div>
     </form>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,40 @@ import CardHeadings from "../components/CardHeadings";
 import Header from "../components/Header";
 import StackedList from "../components/StackedList";
 
+import { collection, getFirestore, orderBy, query } from "firebase/firestore";
+import { useFirestoreCollectionData } from "reactfire";
+
 export default function Home() {
   return (
     <>
       <Header />
       <div className="m-5">
         <CardHeadings />
-        <StackedList />
+        <ListView />
       </div>
     </>
+  );
+}
+
+function ListView() {
+  const threadsV1Col = collection(getFirestore(), "threads-v1");
+  const threadsV1Query = query(threadsV1Col, orderBy("createdAt", "asc"));
+
+  const { status, data: threads } = useFirestoreCollectionData(threadsV1Query, {
+    idField: "id", // this field will be added to the object created from each document
+  });
+
+  if (status === "loading") {
+    return <p>データを取得中...</p>;
+  }
+
+  return (
+    <ul>
+      {threads.map((threadData) => (
+        <li key={threadData.id}>
+          <StackedList title={threadData.title} body={threadData.body} />
+        </li>
+      ))}
+    </ul>
   );
 }
